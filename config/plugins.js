81ea3d0a94c0641af93a5d1d15636c8f463b450a
Bintang Tobing <bintangtobing@theluxeweek.com>
2024-07-26 00:12:24 +0700

    update: plugin mailer settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer", // Use Nodemailer as the email provider
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_FROM", "hello@example.com"), // Default from address
+        defaultReplyTo: env("SMTP_REPLYTO", "hello@example.com"), // Default reply-to address
+        testAddress: env("SMTP_TEST_ADDRESS", "test@example.com"), // Test address (optional)
+      },
+    },
+    logger: {
+      debug: console.log,
+      info: console.info,
+      warn: console.warn,
+      error: console.error,
+    },
+  },
+});
