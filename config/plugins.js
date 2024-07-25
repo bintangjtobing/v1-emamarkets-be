37449d9354710fd49e5811b07cc2c9a12b93c840
Bintang Tobing <bintangtobing@theluxeweek.com>
2024-07-26 00:26:08 +0700

    update: dependencies and add plugins nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
+  // ...
   email: {
-    logger: {
-      debug: console.log,
-      info: console.info,
-      warn: console.warn,
-      error: console.error,
-    },
     config: {
-      provider: "nodemailer",
+      provider: "nodemailer", // Use Nodemailer as the email provider
       providerOptions: {
         host: env("SMTP_HOST", "smtp.example.com"),
         port: env("SMTP_PORT", 587),
@@ -15,11 +10,23 @@ export default ({ env }) => ({
           user: env("SMTP_USERNAME"),
           pass: env("SMTP_PASSWORD"),
         },
+        // Additional Nodemailer options can be added here if needed
       },
       settings: {
-        defaultFrom: env("SMTP_FROM", "hello@example.com"),
-        defaultReplyTo: env("SMTP_REPLYTO", "hello@example.com"),
+        defaultFrom: env("SMTP_FROM", "hello@example.com"), // Default from address
+        defaultReplyTo: env("SMTP_REPLYTO", "hello@example.com"), // Default reply-to address
+        testAddress: env("SMTP_TEST_ADDRESS", "test@example.com"), // Test address (optional)
       },
     },
+    logger: {
+      debug: console.log,
+      info: console.info,
+      warn: console.warn,
+      error: console.error,
+    },
   },
+  seo: {
+    enabled: true,
+  },
+  // ...
 });
