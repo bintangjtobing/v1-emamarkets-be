fdb423304b7749f28e70ccbfc2f30807b9113ce2
Bintang Tobing <hello@bintangtobing.com>
2024-09-08 13:48:19 +0700

    update env dev

---
+++ config/env/development/plugins.js
@@ -0,0 +1,50 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "nodemailer", // Use Nodemailer as the email provider
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // Additional Nodemailer options can be added here if needed
+      },
+      settings: {
+        defaultFrom: env("SMTP_FROM", "hello@example.com"), // Default from address
+        defaultReplyTo: env("SMTP_REPLYTO", "hello@example.com"), // Default reply-to address
+        testAddress: env("SMTP_TEST_ADDRESS", "test@example.com"), // Test address (optional)
+      },
+    },
+    logger: {
+      debug: console.log,
+      info: console.info,
+      warn: console.warn,
+      error: console.error,
+    },
+  },
+  seo: {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: "upload_from_be_strapi",
+          use_filename: true,
+          unique_filename: false,
+        },
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
